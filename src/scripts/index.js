5cafeb517b465d065d30291101e55715a2d1eccc
sharan-cj <sharan.cj.gowda@gmail.com>
2020-03-02 22:04:30 +0530

    getNews and searchKey

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,39 @@
-fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=d08ff880425144bb8d2bf082a55f6d7b')
-.then(
-    function (res){
-        if(res.status !== 200){
-            console.log('Not able to get data :(  Error No', res.status);
-            return;
+const url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=d08ff880425144bb8d2bf082a55f6d7b';
+
+// search input
+let search = document.getElementById('search');
+search.addEventListener('keypress', searchFunction);
+function searchFunction(e){
+    let searchKey = document.getElementById('search').value;
+    if(e.which == 13){
+        console.log(searchKey);
+        if(searchKey != ''){
+            let searchUrl = `https://newsapi.org/v2/everything?q=${searchKey}&apiKey=d08ff880425144bb8d2bf082a55f6d7b`;
+            news(searchUrl);
+        } else {
+            news(url);
         }
-        res.json().then(function(data){
-            console.log(data);
-        });
     }
-);
+
+}
+
+// get news
+async function news(url){
+    const response = await fetch(url);
+    const data = await response.json(); 
+    console.log(data);
+    let output = '';
+    data.articles.forEach(item => {
+        output += `
+            <li class="article">
+                <img class="article-img"  src="${item.urlToImage}">
+                <h2 class="article-title">${item.title}</h2>
+                <p class="article-description">${item.content}</p>
+                <span class="article-author"> - ${item.author}</span>
+                <a class="article-link" href="${item.url}" target="_blank">Read more...</a>
+            </li>
+        `;
+    });
+    document.getElementById('news-articles').innerHTML = output;
+}
+news(url);
